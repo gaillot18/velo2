dfde399fcd467237277f9a9779b9f40eb29bfd4c
Cristian <cche@users.noreply.github.com>
2024-09-27 11:29:55 +0200

    Added run_simulation with numsteps and probs

--- velo_rent.py
+++ velo_rent.py
@@ -1,10 +1,35 @@
-# download modsim.py if necessary
-
-from os.path import basename, exists
-
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
-from modsim import State, TimeSeries, flip
+
+def State(**variables):
+    """Contains the values of state variables."""
+    return pd.Series(variables, name='state')
+
+def flip(p=0.5):
+    """Flips a coin with the given probability.
+
+    p: float 0-1
+
+    returns: boolean (True or False)
+    """
+    return np.random.random() < p
+
+
+def TimeSeries(*args, **kwargs):
+    """Make a pd.Series object to represent a time series.
+    """
+    if args or kwargs:
+        #underride(kwargs, dtype=float)
+        series = pd.Series(*args, **kwargs)
+    else:
+        series = pd.Series([], dtype=float)
+
+    series.index.name = 'Time'
+    if 'name' not in kwargs:
+        series.name = 'Quantity'
+    return series
 
 bikeshare = State(mailly=10, moulin=2)
 
@@ -29,19 +54,22 @@ def step(p1, p2):
         velo_a_moulin()
 
 
-results = TimeSeries()
 
-results[0] = bikeshare.mailly
 
-for i in range(60 * 14 * 30):
-    # print(f"step {i}")
-    step(0.5, 0.5)
-    results[i + 1] = bikeshare.mailly
+def run_simulation(numstep, p1, p2):
+    results = TimeSeries()
+    results[0] = bikeshare.mailly
+    for i in range(numstep):
+        step(p1, p2)
+        results[i + 1] = bikeshare.mailly
+    return results
+
+numstep, p1, p2 = 100, 0.5, 0.47
+res = run_simulation(numstep, p1, p2)
 
 fig, ax = plt.subplots()
-ax.plot(results)
+ax.plot(res)
 ax.set_title("Velos à Mailly")
 ax.set_xlabel("Temps")
 ax.set_ylabel("nombre")
 plt.savefig("mailly.png")
-
