d4cfc9730238e8c15d6b2109a352791c96d8e8e3
Cristian <cche@users.noreply.github.com>
2024-10-03 16:07:23 +0200

    Run one simulation with run_simulation()

--- velo_rent.py
+++ velo_rent.py
@@ -2,51 +2,62 @@
 
 from os.path import basename, exists
 
+import matplotlib.pyplot as plt
+
+from modsim import State, TimeSeries, flip
+
+
+# import functions from modsim
 def download(url):
     filename = basename(url)
     if not exists(filename):
         from urllib.request import urlretrieve
+
         local, _ = urlretrieve(url, filename)
-        print('Downloaded ' + local)
-    
-download('https://github.com/AllenDowney/ModSimPy/raw/master/modsim.py')
+        print(f"Downloaded {local}")
 
-# import functions from modsim
 
-from modsim import *
-import matplotlib.pyplot as plt
+download("https://github.com/AllenDowney/ModSimPy/raw/master/modsim.py")
+
 
 bikeshare = State(mailly=10, moulin=2)
+
 
 def velo_a_mailly():
     # print('Moving a bike to moulin')
     bikeshare.mailly -= 1
     bikeshare.moulin += 1
 
+
 def velo_a_moulin():
     # print('Moving a bike to mailly')
     bikeshare.moulin -= 1
     bikeshare.mailly += 1
 
+
 def step(p1, p2):
     if flip(p1):
         velo_a_mailly()
-    
+
     if flip(p2):
         velo_a_moulin()
 
-results = TimeSeries()
 
-results[0] = bikeshare.mailly
+def run_simulation(num_steps, p1, p2):
+    results = TimeSeries()
+    results[0] = bikeshare.mailly
+    for i in range(num_steps):
+        step(p1, p2)
+        results[i] = bikeshare.mailly
+    return results
 
-for i in range(60*14*30):
-    # print(f"step {i}")
-    step(0.5, 0.4)
-    results[i+1] = bikeshare.mailly
+
+res = run_simulation(10000, 0.5, 0.4)
 
 fig, ax = plt.subplots()
-ax.plot(results)
+ax.plot(res)
 ax.set_title("Velos à Mailly")
 ax.set_xlabel("Temps")
 ax.set_ylabel("nombre")
-plt.show()
+plt.savefig("mailly.png")
+
